2cce19f34e044a78d5de70070d7e2b4262f838ac
Ahmad Ali Khan Shirwany <ahmad.shirwany@tuni.fi>
2023-09-19 13:21:24 +0300

    factors

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -13,22 +13,18 @@ int main()
     }
     else{
      int factor1, factor2;
-     factor2 = 0;
-     factor1 = number;
-     int b = number/2;
-     if (b>10){
-         b = 10;
-     }
-    for ( int i = 1; i <b ; ++i ) {
+     factor2 = number;
+     factor1 = 0;
+    for ( int i = 1; i <number ; ++i ) {
         if (number%i == 0 ){
-            if (number/i <= factor1){
+            if (number/i >= factor2){
                 factor1 = number/i;
                 factor2 = i;
             }
         }
 
     }
-     cout << number << " = "<< factor2 << " * " << factor1 << endl ;
+     cout << number << " = "<< factor1 << " * " << factor2 << endl ;
 
     // Write your code here
     }
