ea431c46436dbda6203e4df63526fb53c1a20758
Ahmad Ali Khan Shirwany <ahmad.shirwany@tuni.fi>
2023-09-19 13:05:23 +0300

    factors

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -5,17 +5,33 @@ using namespace std;
 int main()
 {
     int number = 0;
+
     cout << "Enter a positive number: ";
     cin >> number;
-    for ( int i = 3; i <= number; ++i ) {
-        if (number%i == 0 && number/i < 10){
-            cout << number << " = "<< number/i << " * " << i << endl ;
-            break;
+    if (number <1){
+        cout << "Only positive numbers accepted" << endl;
+    }
+    else{
+     int factor1, factor2;
+     factor1 = number;
+     int b = number/2;
+     if (b>10){
+         b = 10;
+     }
+    for ( int i = 1; i <b ; ++i ) {
+        if (number%i == 0 ){
+            if (number/i <= factor1){
+                factor1 = number/i;
+                factor2 = i;
+            }
         }
 
     }
+     cout << number << " = "<< factor2 << " * " << factor1 << endl ;
 
     // Write your code here
+    }
 
     return 0;
 }
+
