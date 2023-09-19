d0ea4f9e34d5bdea0c4289eac19bf08ecc42e114
Ahmad Ali Khan Shirwany <ahmad.shirwany@tuni.fi>
2023-09-19 13:43:53 +0300

    cube

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    if (number > 0){
+        int cube = number * number * number;
+        if (cube/number/number ==  number){
+            std::cout << "The cube of "<<number<<" is "<< cube <<"."<<endl;
+        }
+        else {
+                std::cout << "Error! The cube of "<<number<<" is not "<< cube<<"." <<endl;
+            }
+
+    }
+    else{
+        std::cout << "Entered number not an positive integer: ";
+    }
+    return 0;
+}
