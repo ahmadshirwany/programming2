05c716346c3e8ac13dd29367ad3c32e305cd0788
Ahmad Ali Khan Shirwany <ahmad.shirwany@tuni.fi>
2023-10-04 01:31:15 +0300

    edit main fuction to run the game

--- student/04/checkers/main.cpp
+++ student/04/checkers/main.cpp
@@ -24,5 +24,57 @@ bool is_digits(string& str)
 
 int main()
 {
+   char pieceChar;
+   cout << "Enter piece character: ";
+   cin >> pieceChar;
+   vector<int> cor ;
+   GameBoard gameb = GameBoard(pieceChar);
+   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+   gameb.print();
+    while (true) {
+       cor.clear();
+       cout << "Enter start and destination points (e.g., 12 34) or q to quit: ";
+       string moveInput ;
+       std::getline(std::cin, moveInput);
+
+       if (moveInput == "q") {
+           gameb.print_moves();
+           break;
+       }
+       if (moveInput.size()!=7){
+           cout<<"Invalid start/destination point."<<endl;
+           continue;
+       }
+       if (!is_digits(moveInput)){
+            cout<<"Invalid start/destination point."<<endl;
+            continue;
+       }
+
+       vector<int> cor ;
+        for (unsigned int i = 0; i < moveInput.size(); i++) {
+            if (moveInput[i] == ' ') {
+                // Ignore spaces
+                continue;
+            }
+            else{
+                cor.push_back( int(moveInput[i]) - 48);
+            }
+        }
+       if (cor.size()!=4){
+            cout<<"Invalid start/destination point."<<endl;
+             continue;
+       }
+       if (gameb.isvalidinput(cor)){
+           cout<<"Invalid start/destination point."<<endl;
+            continue;
+       }
+
+       if (!gameb.isvalidmove(cor)){
+            cout<<"Invalid start/destination point."<<endl;
+             continue;
+       }
+       gameb.makemove(cor);
+    }
+
    return 0;
 }
