4204e54fe425e2356f969172239eec9b795cbd13
Ahmad Ali Khan Shirwany <ahmad.shirwany@tuni.fi>
2023-11-07 02:41:36 +0200

    traffic

---
+++ student/08/traffic/queue.cpp
@@ -0,0 +1,76 @@
+#include "queue.hh"
+#include <iostream>
+
+Queue::Queue(unsigned int cycle) : cycle_(cycle) {
+    // Other attributes are initialized automatically
+}
+
+Queue::~Queue() {
+    while (first_ != nullptr) {
+        Vehicle* temp = first_;
+        first_ = first_->next;
+        delete temp;
+    }
+}
+
+void Queue::enqueue(string const& reg) {
+    if (!is_green_) {
+        Vehicle* newVehicle = new Vehicle{reg, nullptr};
+        if (first_ == nullptr) {
+            first_ = newVehicle;
+            last_ = newVehicle;
+        } else {
+            last_->next = newVehicle;
+            last_ = newVehicle;
+        }
+    }
+    else{
+        cout<<"GREEN: The vehicle "<< reg <<" need not stop to wait"<<endl;
+    }
+}
+
+void Queue::switch_light() {
+    is_green_ = !is_green_;
+    if (is_green_) {
+        if (first_ == nullptr) {
+                   cout << "GREEN: No vehicles waiting in traffic lights" << endl;
+               }
+        else {
+
+        unsigned int vehicles_to_release = cycle_;
+        cout << "GREEN: Vehicle(s) ";
+        while (vehicles_to_release > 0  && first_  != nullptr) {
+            Vehicle* temp = first_;
+            first_ = first_->next;
+            cout <<  temp->reg_num<<" ";
+            delete temp;
+            vehicles_to_release--;
+        }
+        cout <<" can go on" << endl;
+        }
+
+    } else {
+        cout << "RED: No vehicles waiting in traffic lights" << endl;
+    }
+}
+
+void Queue::reset_cycle(unsigned int cycle) {
+    cycle_ = cycle;
+}
+
+void Queue::print() const {
+    cout << (is_green_ ? "GREEN" : "RED") << ": Vehicle(s) ";
+    if (first_ != nullptr) {
+        Vehicle* current = first_;
+        while (current != nullptr) {
+            cout << current->reg_num;
+            if (current->next != nullptr) {
+                cout << " ";
+            }
+            current = current->next;
+        }
+        cout << " waiting in traffic lights" << endl;
+    } else {
+        cout << "No vehicles waiting in traffic lights" << endl;
+    }
+}
